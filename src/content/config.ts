51d6e446740f7e978b5ae4bc02f4916f769a3121
Tianle Cao <caotianlebme@gmail.com>
2026-04-26 01:34:44 -0700

    Implement Submodule Blog setup: add image-stitching repo and update Astro config to handle mixed content

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,9 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   type: "content",
   schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
+    title: z.string().optional(),
+    description: z.string().optional(),
+    date: z.coerce.date().optional(),
     draft: z.boolean().optional()
   }),
 });
